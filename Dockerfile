56dc7c379f1b2681dd1c3fa71403f334eb367c2f
sarthak-m-das <sarthakmdas@gmail.com>
2023-11-21 01:12:34 -0500

    starter

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM jenkins/jenkins:latest
+ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
+ENV CASC_JENKINS_CONFIG /var/jenkins_home/casc.yaml
+COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
+RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
+COPY casc.yaml /var/jenkins_home/casc.yaml
+COPY pipeline.groovy /usr/local/pipeline.groovy
---
+++ pipeline.groovy
@@ -0,0 +1,80 @@
+pipelines = ["auto-pipeline"]
+
+// iterate through the array and call the create_pipeline method
+pipelines.each { pipeline ->
+    println "Creating pipeline ${pipeline}"
+    create_pipeline(pipeline)
+}
+
+// a method that creates a basic pipeline with the given parameter name
+def create_pipeline(String name) {
+    pipelineJob(name) {
+        definition {
+            cps {
+                sandbox(true)
+                script("""
+
+pipeline {
+    agent any
+    stages {
+        stage('Workspace Cleanup') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Github Checkout') {
+            steps {
+                // Checks out the source code from the GitHub repository
+                git branch: 'main', url: 'https://github.com/spring-projects/spring-petclinic'
+            }
+        }
+        
+        stage('Unit Tests') {
+            steps {
+                sh './mvnw test -Dcheckstyle.skip=true'
+            }
+        }
+        
+        // stage('SonarQube Tests') {
+        //     environment {
+        //         scannerHome = tool 'SonarQube'
+        //     }
+        //     steps {
+        //         withSonarQubeEnv('SonarQube') {
+        //             sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=spring-petclinic -Dsonar.java.binaries=target/classes"
+        //         }
+        //         timeout(time: 10, unit: 'MINUTES') {
+        //             waitForQualityGate abortPipeline: true
+        //         }
+        //     }
+        // }
+
+        stage('Build') {
+            steps {
+                // Run the Maven package command
+                sh './mvnw package -DskipTests -Dcheckstyle.skip=true'
+                // Stash the JAR file(s) for use in later stages
+                stash includes: 'target/*.jar', name: 'built-jars'
+            }
+        }
+
+        stage('Execute') {
+            steps {
+                // Restore the stashed JAR file(s)
+                unstash 'built-jars'
+                // Now you can use the JAR file(s) in this stage
+                script {
+                    // Find the JAR file name since the wildcard '*' cannot be used directly with the java command
+                    def jar = sh(script: "ls target/*.jar", returnStdout: true).trim()
+                    sh "JENKINS_NODE_COOKIE=dontKillMe nohup java -jar ${jar} --server.port=8000 &"
+                }
+            }
+        }
+    }
+}
+
+""")
+            }
+        }
+    }
+}
