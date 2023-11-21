f1ad6f03c0a7e9a1d284a029e1bc5995470d9e5d
sarthak-m-das <sarthakmdas@gmail.com>
2023-11-21 17:27:41 -0500

    changes

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ curl --location --request POST "$TOKEN_API_ENDPOINT" \
      --header "$AUTH_HEADER" \
      --data '' >> token.json
 
+export SONARQUBE_TOKEN=$(jq -r '.token' token.json)
 
 echo "Token saved to token.json"
 
 # Webhook API Endpoint
-WEBHOOK_API_ENDPOINT="http://localhost:9000/api/webhooks/create?name=jenkins&url=http%3A%2F%2F10.0.0.14%3A8080%2Fsonarqube-webhook%2F"
 
 curl --location --request POST "$WEBHOOK_API_ENDPOINT" \
     --header 'Authorization: Basic YWRtaW46Yml0c29mUGFwZXI='
